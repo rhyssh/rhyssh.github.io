6ac0d48211975b57b08539a1aa6e3193b7bfe414
rhyssh <reikiaziz30@gmail.com>
2024-03-15 16:21:07 +0700

    update: add page portofolio

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+const link = document.getElementById('link');
+console.log(link);
+
 function showLove() {
   const love = document.createElement("p");
   love.innerHTML = "&#10084;";
   love.classList.add("love-big");
 
   // generate posisi dan rotasi random
-  const posisi = Math.round(event.clientx / window.innerWidth) * 180;
-  const posisi1 = Math.floor(event.clientY / window.innerHeight) * 180;
+  const posisi = Math.round(window.innerWidth / Math.random()) * 180;
+  const posisi1 = Math.floor(window.innerHeight / Math.random()) * 180;
   const rotasi = Math.floor(Math.random() * 360) + 1;
   love.style.translateX = posisi + "%";
   love.style.translateY = posisi1 + "%";
@@ -22,4 +25,25 @@ function showLove() {
       love.remove();
     }, 2000);
   }, 2000);
+}
+
+setTimeout(() => {
+  if (link) {
+    link.style.opacity = 1;
+    link.style.transform = "translateY(30px)"
+    animasi();
+  } else {
+    console.error("Variabel 'link' belum didefinisikan!");
+  }
+}, 1000);
+
+const transformValue = window.getComputedStyle(link).getPropertyValue('transform');
+console.log(transformValue);
+
+function animasi() {
+
+  if (condition) {
+
+  }
+
 }
