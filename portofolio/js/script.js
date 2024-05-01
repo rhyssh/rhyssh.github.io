d7d496e3c5d542b67b5db244a9b0c3b22b606449
rhyssh <reikiaziz30@gmail.com>
2024-05-01 09:35:12 +0700

    docs: tambah bagian dan animasi

--- portofolio/js/script.js
+++ portofolio/js/script.js
@@ -5,6 +5,13 @@ const statusi = document.getElementsByClassName('name-1')
 const hamburger = document.getElementById('hamburger-icon')
 const hamburgerMenu = document.getElementById('hamburger-menu')
 const menuMobile = document.getElementById('menu-mobile');
+const skillButton = document.getElementById('menu-skill')
+const toolButton = document.getElementById('menu-tool')
+const skillsContent = document.getElementById('skills')
+const toolsContent = document.getElementById('tools')
+const skillContainer = document.getElementById('skill-bungkus')
+const toolContainer = document.getElementById('tool-bungkus')
+let currIndex = 0
 console.log(pp);
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -38,5 +45,50 @@ hamburger.addEventListener('click', () => {
 
         // hamburger.classList.add('animate__animated', 'animate__rotateOut')
 
+    }
+});
+
+
+skillButton.addEventListener('click', function () {
+    if (currIndex !== 0) {
+        toolButton.classList.remove('bg-[#e6c16b42]')
+        skillButton.classList.add('bg-[#e6c16b42]')
+
+        // animasi
+        toolContainer.classList.add('animate__animated', 'animate__fadeOutRightBig')
+
+
+        // content
+        setTimeout(() => {
+            skillsContent.classList.remove('hidden')
+            toolsContent.classList.add('hidden')
+            toolContainer.classList.remove('animate__animated', 'animate__fadeOutRightBig')
+            skillContainer.classList.add('animate__animated', 'animate__fadeInLeftBig')
+        }, 90);
+
+        // current
+        currIndex = 0
+    }
+});
+
+toolButton.addEventListener('click', function () {
+    if (currIndex !== 1) {
+        skillButton.classList.remove('bg-[#e6c16b42]')
+        toolButton.classList.add('bg-[#e6c16b42]')
+
+        // animasi
+        skillContainer.classList.add('animate__animated', 'animate__fadeOutLeftBig')
+
+
+        // content
+        setTimeout(() => {
+            toolsContent.classList.remove('hidden')
+            skillsContent.classList.add('hidden')
+            skillContainer.classList.remove('animate__animated', 'animate__fadeOutLeftBig')
+            toolContainer.classList.add('animate__animated', 'animate__fadeInRightBig')
+        }, 90);
+
+        // curindex
+        currIndex = 1
     }
 });
