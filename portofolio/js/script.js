a26fb2807618de10bd3e28692c571a0c20a50e5c
rhyssh <reikiaziz30@gmail.com>
2024-05-02 16:54:08 +0700

    docs:update tampilan

--- portofolio/js/script.js
+++ portofolio/js/script.js
@@ -11,8 +11,16 @@ const skillsContent = document.getElementById('skills')
 const toolsContent = document.getElementById('tools')
 const skillContainer = document.getElementById('skill-bungkus')
 const toolContainer = document.getElementById('tool-bungkus')
-let currIndex = 0
-console.log(pp);
+const webButton = document.getElementById('web-button')
+const designButton = document.getElementById('design-button')
+const galleryButton = document.getElementById('gallery-button')
+const webContainer = document.getElementById('web-bungkus')
+const designContainer = document.getElementById('design-bungkus')
+const galleryContainer = document.getElementById('gallery-bungkus')
+
+let currIndexskill = 0
+let currIndexproject = 0
+// console.log(pp);
 
 document.addEventListener('DOMContentLoaded', function () {
     pp.classList.add('animate__animated', 'animate__fadeInRight');
@@ -50,13 +58,12 @@ hamburger.addEventListener('click', () => {
 
 
 skillButton.addEventListener('click', function () {
-    if (currIndex !== 0) {
+    if (currIndexskill !== 0) {
         toolButton.classList.remove('bg-[#e6c16b42]')
         skillButton.classList.add('bg-[#e6c16b42]')
 
         // animasi
         toolContainer.classList.add('animate__animated', 'animate__fadeOutRightBig')
-
 
         // content
         setTimeout(() => {
@@ -67,18 +74,17 @@ skillButton.addEventListener('click', function () {
         }, 90);
 
         // current
-        currIndex = 0
+        currIndexskill = 0
     }
 });
 
 toolButton.addEventListener('click', function () {
-    if (currIndex !== 1) {
+    if (currIndexskill !== 1) {
         skillButton.classList.remove('bg-[#e6c16b42]')
         toolButton.classList.add('bg-[#e6c16b42]')
 
         // animasi
         skillContainer.classList.add('animate__animated', 'animate__fadeOutLeftBig')
-
 
         // content
         setTimeout(() => {
@@ -89,6 +95,37 @@ toolButton.addEventListener('click', function () {
         }, 90);
 
         // curindex
-        currIndex = 1
+        currIndexskill = 1
     }
 });
+
+function ganti(target, targetContent, teman, saudara, temanContent, saudaraContent, index) {
+
+    if (currIndexproject !== index) {
+        teman.classList.remove('bg-[#e6c16b42]')
+        saudara.classList.remove('bg-[#e6c16b42]')
+        target.classList.add('bg-[#e6c16b42]')
+
+        console.log(currIndexproject)
+
+
+        setTimeout(() => {
+            targetContent.classList.remove('hidden')
+            temanContent.classList.add('hidden')
+            saudaraContent.classList.add('hidden')
+        }, 90);
+
+        currIndexproject = index
+    }
+}
+webButton.addEventListener('click', () => {
+    ganti(webButton, webContainer, designButton, galleryButton, designContainer, galleryContainer, 0)
+})
+
+designButton.addEventListener('click', () => {
+    ganti(designButton, designContainer, webButton, galleryButton, webContainer, galleryContainer, 1)
+})
+
+galleryButton.addEventListener('click', () => {
+    ganti(galleryButton, galleryContainer, webButton, designButton, webContainer, designContainer, 2)
+})
